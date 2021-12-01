466fa555b9e8fce11b25ff16f7531902b21834e8
vininribeiro23 <vininribeiro@hotmail.com>
2021-12-01 12:07:51 -0300

    primeiro commit

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://vininribeiro:32616144Vinicius@cluster0.l7orv.mongodb.net/NodeBD2?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://<Password>@cluster0.l7orv.mongodb.net/NodeBD2?retryWrites=true&w=majority');
 mongoose.Promise =  global.Promise;
 
 
